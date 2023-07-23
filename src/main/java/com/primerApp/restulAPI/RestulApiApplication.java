f3759965261e7781dfef918309508bc064ac7138
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-07-23 19:18:31 -0300

    RestulAPI proyect started

---
+++ src/main/java/com/primerApp/restulAPI/RestulApiApplication.java
@@ -0,0 +1,13 @@
+package com.primerApp.restulAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primerApp/restulAPI/RestulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primerApp.restulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
