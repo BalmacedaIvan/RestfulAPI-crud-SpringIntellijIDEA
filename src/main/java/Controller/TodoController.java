195511bd1e5a13ff2b2f8494b8144a7ab93f43bc
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-07-25 12:52:30 -0300

    Delete method in TodoController

--- src/main/java/Controller/TodoController.java
+++ src/main/java/Controller/TodoController.java
@@ -3,11 +3,9 @@ package Controller;
 import Model.Task;
 import Repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -27,5 +25,21 @@ public class TodoController {
         return "Task was saved";
     }
 
-    
+    @PutMapping(value = "/update/{id}")
+    public String updateTask(@PathVariable Long id, @RequestBody Task task){
+        Task updatedTask = todoRepository.findAllById(id).get();
+        updatedTask.setTitle(task.getTitle());
+        updatedTask.setDesc(task.getDesc());
+
+        todoRepository.save(updatedTask);
+        return "Task was updated";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteTask(@PathVariable Long id){
+        Task delTask = todoRepository.findAllById(id).get();
+        todoRepository.delete(delTask);
+
+        return "Task was deleted";
+    }
 }
