01bb3c35ed75380877267a47884a93440a00bc0e
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-07-25 11:56:53 -0300

    I added MySql dependency and created Task class as Entity whit annotations

--- src/main/java/Model/Task.java
+++ src/main/java/Model/Task.java
@@ -1,2 +1,39 @@
-package Model;public class Task {
+package Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(schema="TasksCrud")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String title;
+    @Column
+    private String desc;
+    @Column
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 }
