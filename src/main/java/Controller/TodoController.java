36be51ab96c66e989bb34c5d3779b2aea6214fa3
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-07-25 12:42:03 -0300

    Post method in TodoController

--- src/main/java/Controller/TodoController.java
+++ src/main/java/Controller/TodoController.java
@@ -4,6 +4,8 @@ import Model.Task;
 import Repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,4 +20,12 @@ public class TodoController {
     public List<Task> getTasks(){
         return todoRepository.findAll();
     }
+
+    @PostMapping(value = "/saveTask")
+    public String postTask(@RequestBody Task task){
+        todoRepository.save(task);
+        return "Task was saved";
+    }
+
+    
 }
